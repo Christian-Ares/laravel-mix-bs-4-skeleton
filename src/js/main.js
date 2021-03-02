2ada3c392be31dbc2cd74a712dcad0beec1baae2
Christian Ares <christianaresvillan@gmail.com>
2021-03-02 21:58:21 +0100

    Project init, buttons working, slides added

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,29 @@
 import $ from "jquery";
 import "popper.js";
 import "bootstrap";
+import Swiper from 'swiper';
+import SwiperCore, { Navigation } from 'swiper/core'
+
+SwiperCore.use([Navigation])
+
 
 $(document).ready(function () {
     $('[data-toggle="tooltip"]').tooltip()
 })
+
+const swiper = new Swiper('.swiper-container', {
+  slidesPerView: 1,
+  spaceBetween: 0,
+  direction: 'horizontal',
+  loop: true,
+  effect: 'cube',
+  allowSlideNext: true,
+  allowSlidePrev: true,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+
+swiper.slideNext(500, true);
+swiper.slidePrev(500, true);
